06eb0473fe525bcdef0529174b83ebab6e999a10
Carl Jones <JonesC162@cardiff.ac.uk>
2020-06-18 13:15:41 +0000

    updated controller

--- demo/src/main/java/com/example/sb2/demo/web/HelloController.java
+++ demo/src/main/java/com/example/sb2/demo/web/HelloController.java
@@ -1,11 +1,19 @@
 package com.example.sb2.demo.web;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class HelloController {
 
 @GetMapping("/")
 public String sayHello(){
+    
+    System.out.println("-------------------------");
+    System.out.println("-- in Hello Controller --");
+    System.out.println("-------------------------");
+    
+
     return "HELLO!";
 }
 
